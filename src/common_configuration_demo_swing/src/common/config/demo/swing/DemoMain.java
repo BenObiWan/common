3108830d7cbc67d9f6d1c796be2306e63cb0c9a2
BenObiWan <benobiwan@gmail.com>
2011-10-04 22:56:58 +0200

    Added a tab for the Double configuration.

--- src/common_configuration_demo_swing/src/common/config/demo/swing/DemoMain.java
+++ src/common_configuration_demo_swing/src/common/config/demo/swing/DemoMain.java
@@ -7,18 +7,31 @@ import javax.management.MBeanServer;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
+import common.config.demo.DoubleDemoConfiguration;
 import common.config.demo.FloatDemoConfiguration;
 import common.config.demo.IntegerDemoConfiguration;
 import common.config.swing.ConfigurationPanel;
 
+/**
+ * Main frame for the configuration demo. Creates a panel for each demo
+ * configuration and displays them in tabs.
+ * 
+ * @author benobiwan
+ * 
+ */
 public final class DemoMain extends JFrame
 {
-
 	/**
 	 * serialVersionUID for Serialization.
 	 */
 	private static final long serialVersionUID = -1271806634110676749L;
 
+	/**
+	 * Creates a new DemoMain.
+	 * 
+	 * @throws HeadlessException
+	 *             if GraphicsEnvironment.isHeadless() returns true.
+	 */
 	public DemoMain() throws HeadlessException
 	{
 		super("Configuration demo");
@@ -26,12 +39,15 @@ public final class DemoMain extends JFrame
 				.getPlatformMBeanServer();
 
 		final JTabbedPane contentPane = new JTabbedPane();
+		setContentPane(contentPane);
+
 		contentPane.addTab("Integer", new ConfigurationPanel(
 				new IntegerDemoConfiguration(null, beanServer)));
 		contentPane.addTab("Float", new ConfigurationPanel(
 				new FloatDemoConfiguration(null, beanServer)));
+		contentPane.addTab("Double", new ConfigurationPanel(
+				new DoubleDemoConfiguration(null, beanServer)));
 
-		setContentPane(contentPane);
 	}
 
 	/**
