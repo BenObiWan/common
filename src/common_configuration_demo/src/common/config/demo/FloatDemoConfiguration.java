45f602d2051f108986e9738eac00e47e2c7fdc11
BenObiWan <benobiwan@gmail.com>
2011-10-04 22:51:56 +0200

    Added a TextField configuration leaf in FloatDemoConfiguration.

--- src/common_configuration_demo/src/common/config/demo/FloatDemoConfiguration.java
+++ src/common_configuration_demo/src/common/config/demo/FloatDemoConfiguration.java
@@ -7,12 +7,23 @@ import common.config.IConfiguration;
 import common.config.display.FloatDisplayType;
 import common.config.leaf.ConfigurationFloat;
 
+/**
+ * Configuration demo using Float values.
+ * 
+ * @author benobiwan
+ * 
+ */
 public final class FloatDemoConfiguration extends AbstractConfigurationBranch
 {
 	/**
 	 * Leaf configuring a spinner.
 	 */
 	private final ConfigurationFloat _leafSpinner;
+
+	/**
+	 * Leaf configuring a spinner.
+	 */
+	private final ConfigurationFloat _leafTextField;
 
 	/**
 	 * Creates a new IntegerDemoConfiguration.
@@ -30,8 +41,14 @@ public final class FloatDemoConfiguration extends AbstractConfigurationBranch
 				"Spinner demo", "Spinner demo.", "Invalid spinner demo value",
 				false, FloatDisplayType.SPINNER, Float.valueOf(1024),
 				Float.valueOf(65535), Float.valueOf(1664));
-
 		addLeaf(_leafSpinner);
+
+		_leafTextField = new ConfigurationFloat(this, "Text field demo",
+				"Text field demo", "Text field demo.",
+				"Invalid text field demo value", false,
+				FloatDisplayType.TEXTFIELD, Float.valueOf(1024),
+				Float.valueOf(65535), Float.valueOf(1664));
+		addLeaf(_leafTextField);
 	}
 
 	@Override
