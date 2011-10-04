5659c3e042bf7921ddca80b1a532d4bf1da78d4d
BenObiWan <benobiwan@gmail.com>
2011-10-04 22:53:47 +0200

    Added a TextField and Slider configuration leaf in IntegerDemoConfiguration.

--- src/common_configuration_demo/src/common/config/demo/IntegerDemoConfiguration.java
+++ src/common_configuration_demo/src/common/config/demo/IntegerDemoConfiguration.java
@@ -7,12 +7,28 @@ import common.config.IConfiguration;
 import common.config.display.IntegerDisplayType;
 import common.config.leaf.ConfigurationInteger;
 
+/**
+ * Configuration demo using Integer values.
+ * 
+ * @author benobiwan
+ * 
+ */
 public final class IntegerDemoConfiguration extends AbstractConfigurationBranch
 {
 	/**
 	 * Leaf configuring a spinner.
 	 */
 	private final ConfigurationInteger _leafSpinner;
+
+	/**
+	 * Leaf configuring a slider.
+	 */
+	private final ConfigurationInteger _leafSlider;
+
+	/**
+	 * Leaf configuring a text field.
+	 */
+	private final ConfigurationInteger _leafTextField;
 
 	/**
 	 * Creates a new IntegerDemoConfiguration.
@@ -26,12 +42,24 @@ public final class IntegerDemoConfiguration extends AbstractConfigurationBranch
 			final MBeanServer mBeanServer)
 	{
 		super(parent, "Integer configuration demo", mBeanServer);
+
 		_leafSpinner = new ConfigurationInteger(this, "Spinner demo",
 				"Spinner demo", "Spinner demo.", "Invalid spinner demo value",
 				false, IntegerDisplayType.SPINNER, Integer.valueOf(1024),
 				Integer.valueOf(65535), Integer.valueOf(1664));
-
 		addLeaf(_leafSpinner);
+
+		_leafSlider = new ConfigurationInteger(this, "Slider demo",
+				"Slider demo", "Slider demo.", "Invalid slider demo value",
+				false, IntegerDisplayType.SLIDER, Integer.valueOf(1024),
+				Integer.valueOf(65535), Integer.valueOf(1664));
+		addLeaf(_leafSlider);
+
+		_leafTextField = new ConfigurationInteger(this, "Text field demo",
+				"Slider demo", "Slider demo.", "Invalid slider demo value",
+				false, IntegerDisplayType.TEXTFIELD, Integer.valueOf(1024),
+				Integer.valueOf(65535), Integer.valueOf(1664));
+		addLeaf(_leafTextField);
 	}
 
 	@Override
