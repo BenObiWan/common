e3341d049d07e773845e43cb224e8058d266924c
BenObiWan <benobiwan@gmail.com>
2011-11-06 17:51:10 +0100

    Added TreeCheckBoxCellEditor and TreeCheckBoxCellRenderer.

---
+++ src/common_logging_swing/src/common/swing/TreeCheckBoxCellEditor.java
@@ -0,0 +1,28 @@
+package common.swing;
+
+import java.awt.Component;
+
+import javax.swing.AbstractCellEditor;
+import javax.swing.JTree;
+import javax.swing.tree.TreeCellEditor;
+
+public class TreeCheckBoxCellEditor extends AbstractCellEditor implements
+		TreeCellEditor
+{
+
+	@Override
+	public Object getCellEditorValue()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Component getTreeCellEditorComponent(JTree tree, Object value,
+			boolean isSelected, boolean expanded, boolean leaf, int row)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/common_logging_swing/src/common/swing/TreeCheckBoxCellRenderer.java
@@ -0,0 +1,25 @@
+package common.swing;
+
+import java.awt.Component;
+
+import javax.swing.JTree;
+import javax.swing.tree.TreeCellRenderer;
+
+public class TreeCheckBoxCellRenderer implements TreeCellRenderer
+{
+
+	public TreeCheckBoxCellRenderer()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Component getTreeCellRendererComponent(JTree tree, Object value,
+			boolean selected, boolean expanded, boolean leaf, int row,
+			boolean hasFocus)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
