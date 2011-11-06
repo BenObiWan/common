97234d5a5664147e1fe109d7573e0c6a44f01286
BenObiWan <benobiwan@gmail.com>
2011-11-06 20:43:37 +0100

    Added Javadoc.

--- src/common_logging_swing/src/common/swing/DisposableInternalFrame.java
+++ src/common_logging_swing/src/common/swing/DisposableInternalFrame.java
@@ -2,6 +2,12 @@ package common.swing;
 
 import javax.swing.JInternalFrame;
 
+/**
+ * {@link JInternalFrame} implementing the {@link IDisposableFrame} interface.
+ * 
+ * @author benobiwan
+ * 
+ */
 public final class DisposableInternalFrame extends JInternalFrame implements
 		IDisposableFrame
 {
@@ -22,50 +28,87 @@ public final class DisposableInternalFrame extends JInternalFrame implements
 	/**
 	 * Creates a non-resizable, non-closable, non-maximizable, non-iconifiable
 	 * JInternalFrame with the specified title.
+	 * 
+	 * @param strTitle
+	 *            the title of the frame.
 	 */
-	public DisposableInternalFrame(final String title)
+	public DisposableInternalFrame(final String strTitle)
 	{
-		super(title);
+		super(strTitle);
 	}
 
 	/**
 	 * Creates a non-closable, non-maximizable, non-iconifiable JInternalFrame
 	 * with the specified title and resizability.
+	 * 
+	 * @param strTitle
+	 *            the title of the frame.
+	 * @param bResizable
+	 *            whether or not the frame is resizable.
 	 */
-	public DisposableInternalFrame(final String title, final boolean resizable)
+	public DisposableInternalFrame(final String strTitle,
+			final boolean bResizable)
 	{
-		super(title, resizable);
+		super(strTitle, bResizable);
 	}
 
 	/**
 	 * Creates a non-maximizable, non-iconifiable JInternalFrame with the
 	 * specified title, resizability, and closability.
+	 * 
+	 * @param strTitle
+	 *            the title of the frame.
+	 * @param bResizable
+	 *            whether or not the frame is resizable.
+	 * @param bClosable
+	 *            whether or not the frame is closable.
 	 */
-	public DisposableInternalFrame(final String title, final boolean resizable,
-			final boolean closable)
+	public DisposableInternalFrame(final String strTitle,
+			final boolean bResizable, final boolean bClosable)
 	{
-		super(title, resizable, closable);
+		super(strTitle, bResizable, bClosable);
 	}
 
 	/**
 	 * Creates a non-iconifiable JInternalFrame with the specified title,
 	 * resizability, closability, and maximizability.
+	 * 
+	 * @param strTitle
+	 *            the title of the frame.
+	 * @param bResizable
+	 *            whether or not the frame is resizable.
+	 * @param bClosable
+	 *            whether or not the frame is closable.
+	 * @param bMaximizable
+	 *            whether or not the frame is maximizable.
 	 */
-	public DisposableInternalFrame(final String title, final boolean resizable,
-			final boolean closable, final boolean maximizable)
+	public DisposableInternalFrame(final String strTitle,
+			final boolean bResizable, final boolean bClosable,
+			final boolean bMaximizable)
 	{
-		super(title, resizable, closable, maximizable);
+		super(strTitle, bResizable, bClosable, bMaximizable);
 	}
 
 	/**
 	 * Creates a JInternalFrame with the specified title, resizability,
 	 * closability, maximizability, and iconifiability.
+	 * 
+	 * @param strTitle
+	 *            the title of the frame.
+	 * @param bResizable
+	 *            whether or not the frame is resizable.
+	 * @param bClosable
+	 *            whether or not the frame is closable.
+	 * @param bMaximizable
+	 *            whether or not the frame is maximizable.
+	 * @param bIconifiable
+	 *            whether or not the frame is iconifiable.
 	 */
-	public DisposableInternalFrame(final String title, final boolean resizable,
-			final boolean closable, final boolean maximizable,
-			final boolean iconifiable)
+	public DisposableInternalFrame(final String strTitle,
+			final boolean bResizable, final boolean bClosable,
+			final boolean bMaximizable, final boolean bIconifiable)
 	{
-		super(title, resizable, closable, maximizable, iconifiable);
+		super(strTitle, bResizable, bClosable, bMaximizable, bIconifiable);
 	}
 
 }
