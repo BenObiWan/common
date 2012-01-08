3e8c88c7203aedc2f0afb01cab570d692dfb7046
BenObiWan <benobiwan@gmail.com>
2012-01-08 01:20:05 +0100

    Javadoc.

--- src/common_swing/src/common/swing/IDisposableFrame.java
+++ src/common_swing/src/common/swing/IDisposableFrame.java
@@ -1,6 +1,15 @@
 package common.swing;
 
+/**
+ * Interface describing a disposable frame.
+ * 
+ * @author benobiwan
+ * 
+ */
 public interface IDisposableFrame
 {
+	/**
+	 * Dispose of this frame.
+	 */
 	void dispose();
 }
